e63d1d4d986e26fa03ae164c8c2981929061872f
saisujithsirigiri <ssirigiri@albany.edu>
2020-04-23 22:04:18 -0400

    Revamped the password reset pages

--- web-application/angular/src/app/reset-password-page/reset-password-page.component.ts
+++ web-application/angular/src/app/reset-password-page/reset-password-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CheckJwtService } from '../shared/check-jwt.service';
 import { ResetPasswordService } from '../shared/reset-password.service';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password-page',
@@ -23,9 +24,15 @@ export class ResetPasswordPageComponent implements OnInit {
   token: string = '';
   passwordPattern = "(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!,@,#,$,%,^,&,*]).{6,20}";
 
+  Form = this.formBuilderService.group({
+    password: ["", Validators.required],
+    rePassword: ["", Validators.required],
+  });
+
   constructor(private route: ActivatedRoute,
     private service : CheckJwtService,
     private resetPasswordService: ResetPasswordService,
+    private formBuilderService: FormBuilder,
     private router: Router) { }
 
   ngOnInit() {
@@ -50,10 +57,11 @@ export class ResetPasswordPageComponent implements OnInit {
   }
 
   onSubmission(){
-      if(!(this.password === this.rePassword)){
+      console.log(this.Form.value)
+      if(!(this.Form.value.password === this.Form.value.rePassword)){
         this.errorVisible = true;
       }else {
-        this.resetPasswordService.makePasswordChange(this.token,this.password).subscribe(
+        this.resetPasswordService.makePasswordChange(this.token,this.Form.value.password).subscribe(
           response => {
             this.errorVisible = false;
             this.visible = !this.visible;
