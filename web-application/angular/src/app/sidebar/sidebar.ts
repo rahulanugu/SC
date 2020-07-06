b2229884671dccd4cfc6d62d6779f3b66ba96486
Srikar_Pothumahanti <srikarrohit@gmail.com>
2020-07-06 19:46:58 -0400

    sidebar component added

---
+++ web-application/angular/src/app/sidebar/sidebar.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'sidenav',
+    templateUrl: 'sidebar.html',
+    styleUrls: ['sidebar.css'],
+  })
+export class SideBarComponent {}
