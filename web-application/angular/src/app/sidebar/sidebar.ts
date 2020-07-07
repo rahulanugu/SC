2c542717e52fff0b195ba912cbf1ee66cac2c44e
Srikar_Pothumahanti <srikarrohit@gmail.com>
2020-07-06 21:09:11 -0400

    sidebar being rendering but need to embed

--- web-application/angular/src/app/sidebar/sidebar.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from '@angular/core';
-@Component({
-    selector: 'sidenav',
-    templateUrl: 'sidebar.html',
-    styleUrls: ['sidebar.css'],
-  })
-export class SideBarComponent {}
