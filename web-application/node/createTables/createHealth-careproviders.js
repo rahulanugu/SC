d154f4e904e20dce2382d52bbcbd18fcfc9467f4
Srikar_Pothumahanti <srikarrohit@gmail.com>
2020-07-09 21:54:07 -0400

    more tables migrated

---
+++ web-application/node/createTables/createHealth-careproviders.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+const {BigQuery} = require('@google-cloud/bigquery');
+const options = {
+    keyFilename: '/Users/srikarpothumahanti/Desktop/scriptchain/web-application/node/serviceAccountKeys/scriptchainprod-96d141251382.json',
+    projectId: 'scriptchainprod'
+
+};
+const bigquery = new BigQuery(options);
+createTable();
+async function createTable() {
+
+    const datasetId = "ScriptChain";
+    const tableId = "healthcareproviders";
+    const schema = 'id:string, email:string, password:string, version:integer';
+ 
+  const options = {
+    schema: schema,
+    location: 'US',
+  };
+
+  // Create a new table in the dataset
+  const [table] = await bigquery
+    .dataset(datasetId)
+    .createTable(tableId, options);
+  console.log(`Table ${table.id} created.`);
+}
