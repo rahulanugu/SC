4726b18379a450ecab149c02adb0dac352ea51bc
Srikar_Pothumahanti <srikarrohit@gmail.com>
2020-07-09 21:54:07 -0400

    more tables migrated

---
+++ web-application/node/createTables/createPatients.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+const {BigQuery} = require('@google-cloud/bigquery');
+const options = {
+    keyFilename: '/Users/srikarpothumahanti/Desktop/scriptchain/web-application/node/serviceAccountKeys/scriptchainprod-96d141251382.json',
+    projectId: 'scriptchainprod'
+
+};
+const bigquery = new BigQuery(options);
+createTable();
+async function createTable() {
+
+    const datasetId = "ScriptChain";
+    const tableId = "patients";
+    const schema = 'id:string, fname:string, lname:string, Email:string, address:string,phone:string,\
+    birthday:string,sex:string,ssn:string,allergies:string,ec:string,ecPhone:string,ecRelationship:string,\
+    password:string,anemia:boolean,asthma:boolean,arthritis:boolean,cancer:boolean,gout:boolean,diabetes:boolean,\
+    emotionalDisorder:boolean,epilepsy:boolean,fainting:boolean,gallstones:boolean,heartDisease:boolean,\
+    heartAttack:boolean,rheumaticFever:boolean,highBP:boolean,digestiveProblems:boolean,ulcerative:boolean,\
+    ulcerDisease:boolean,hepatitis:boolean,kidneyDiseases:boolean,liverDisease:boolean,sleepApnea:boolean,\
+    papMachine:boolean,thyroid:boolean,tuberculosis:boolean,venereal:boolean,neurologicalDisorders:boolean,\
+    bleedingDisorders:boolean,lungDisease:boolean,emphysema:boolean,none:boolean,drink:string,smoke:string,\
+    version:integer';
+
+  const options = {
+    schema: schema,
+    location: 'US',
+  };
+
+  // Create a new table in the dataset
+  const [table] = await bigquery
+    .dataset(datasetId)
+    .createTable(tableId, options);
+  console.log(`Table ${table.id} created.`);
+}
---
+++ web-application/node/insertData/insertIntoVerifiedUser.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const {BigQuery} = require('@google-cloud/bigquery');
+const options = {
+    keyFilename: '/Users/srikarpothumahanti/Desktop/scriptchain/web-application/node/serviceAccountKeys/scriptchainprod-96d141251382.json',
+    projectId: 'scriptchainprod'
+
+};
+const bigquery = new BigQuery(options);
+loadLocalFile();
+async function loadLocalFile() {
+  // Imports a local file into a table.
+
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+   const filename = '/Users/srikarpothumahanti/Desktop/scriptchain/web-application/node/MongoJsonNLFiles/verifieduser.json';
+   const datasetId = 'ScriptChain';
+   const tableId = 'verifieduser';
+
+  // Load data from a local file into the table
+  const [job] = await bigquery
+    .dataset(datasetId)
+    .table(tableId)
+    .load(filename);
+
+  console.log(`Job ${job.id} completed.`);
+
+  // Check the job's status for errors
+  const errors = job.status.errors;
+  if (errors && errors.length > 0) {
+    throw errors;
+  }
+}
