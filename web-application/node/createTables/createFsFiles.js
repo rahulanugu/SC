3afaa455bf96e071827ad8dfe3b6780214001bc8
Srikar_Pothumahanti <srikarrohit@gmail.com>
2020-07-14 09:20:28 -0400

    changes done

---
+++ web-application/node/createTables/createFsFiles.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const {BigQuery} = require('@google-cloud/bigquery');
+const options = {
+    keyFilename: '/Users/srikarpothumahanti/Desktop/scriptchain/web-application/node/serviceAccountKeys/scriptchainprod-96d141251382.json',
+    projectId: 'scriptchainprod'
+
+};
+
+const bigquery = new BigQuery(options);
+createTable();
+async function createTable() {
+
+    const datasetId = "ScriptChain";
+    const tableId = "fsFiles";
+    const schema = 'id:string, length:integer, chunkSize:integer, uploadDate:string, filename:string,md5:string,contentType:string';
+
+  const options = {
+    schema: schema,
+    location: 'US',
+  };
+
+  // Create a new table in the dataset
+  const [table] = await bigquery
+    .dataset(datasetId)
+    .createTable(tableId, options);
+  console.log(`Table ${table.id} created.`);
+}
