ac6931de264dc5acb59bb5857e85cff9298c1ab9
Srikar_Pothumahanti <srikarrohit@gmail.com>
2020-07-20 23:40:10 -0400

    Patient registration/login flow testing and security added

--- web-application/node/controllers/verifiedController.js
+++ web-application/node/controllers/verifiedController.js
@@ -7,7 +7,9 @@ const { Patient } = require('../models/user');
 const { VerifiedUser } = require('../models/verifiedUser');
 var Utility = require('../utility');
 var jwtDecode = require('jwt-decode');
+const fs = require('fs');
 const {BigQuery} = require('@google-cloud/bigquery');
+const { pathToFileURL } = require('url');
 const options = {
     keyFilename: '/Users/srikarpothumahanti/Desktop/scriptchain/web-application/node/serviceAccountKeys/scriptchainprod-96d141251382.json',
     projectId: 'scriptchainprod'
@@ -45,6 +47,16 @@ const bigquery = new BigQuery(options);
 //     }
 // );
 
+function generateId(count) {
+    var _sym = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    var str = '';
+
+    for(var i = 0; i < count; i++) {
+        str += _sym[parseInt(Math.random() * (_sym.length))];
+    }
+    return str;
+}
+
 router.post('/',async(req, res) => {
 
     console.log("Creating an actual user after verification in the database");
@@ -63,18 +75,24 @@ router.post('/',async(req, res) => {
     //decoding the token
     var decodedValue = jwtDecode(decryptedToken);
 
-    const query = 'SELECT * FROM `scriptchainprod.ScriptChain.verifieduser` WHERE email='+'"'+
-    decodedValue.tokeBody.email+'"';
-    bigquery.query(query, function(err, checkCurrentSubscriber) {
+    const query = 'SELECT * FROM `scriptchainprod.ScriptChain.verifieduser` WHERE email=@email';
+    const bigQueryOptions={
+        query:query,
+        location:'US',
+        params:{email:decodedValue.tokeBody.email}
+    }
+    bigquery.query(bigQueryOptions, function(err, checkCurrentSubscriber) {
       if (!err) {
-        if (checkCurrentSubscriber){
-            return res.status(400).send('Subscriber already exists')
+        if (checkCurrentSubscriber.length>0){
+            return res.json('Subscriber already Exists')
         }
-      }
+    }else{
+        res.status(500).json({message:"DB Error"});
+    }
+      
     });
 
 
-    console.log('checkCurrentSubscriber',checkCurrentSubscriber);
     //encrypt the password
     const salt = await bcrypt.genSaltSync(10);
     const hashpassword = await bcrypt.hash(decodedValue.tokeBody.password, salt);
@@ -82,44 +100,42 @@ router.post('/',async(req, res) => {
     //add new patient
     const patient = decodedValue.tokeBody;
     patient['password'] = hashpassword;
-
+    patient['_id'] = generateId(10);
+    patient['Email'] =  new String(patient['email']);
+    delete patient['street'];
+    delete patient['city'];
+    delete patient['state'];
+    delete patient['zip'];
+    delete patient['country'];
+    delete patient['email'];
+    
     const verifieduser = {
+        _id: generateId(10),
         fname: decodedValue.tokeBody.fname,
         lname: decodedValue.tokeBody.lname,
-        email: decodedValue.tokeBody.email,
+        email: patient['Email'],
     };
 
-    bigquery.query(query1, function(err, row1) {
-        const filename = 'verifieduserTmp.json';
-        const datasetId = 'ScriptChain';
-        const tableId = 'verifieduser';
-        fs.writeFileSync(filename, JSON.stringify(verifieduser));         
-        const table = bigquery.dataset(datasetId).table(tableId);
-        // Check the job's status for errors
-        //const errors = job.status.errors;
-        table.load(filename,(err,res) =>{
-            if(err){
-                console.log(err)
-            }
-        });
+    const filename1 = 'verifieduserTmp.json';
+    const datasetId = 'ScriptChain';
+    const tableId1 = 'verifieduser';
+    fs.writeFileSync(filename1, JSON.stringify(verifieduser));         
+    const table1 = bigquery.dataset(datasetId).table(tableId1);
+    // Check the job's status for errors
+    //const errors = job.status.errors;
+    table1.load(filename1,(err1,res1) =>{
     });
 
-    bigquery.query(query2, function(err, row1) {
-        const filename = 'patientTmp.json';
-        const datasetId = 'ScriptChain';
-        const tableId = 'patients';
-        fs.writeFileSync(filename, JSON.stringify(patient));         
-        const table = bigquery.dataset(datasetId).table(tableId);
-        table.load(filename,(err,res) =>{
-            if (!err) {
-                // returns saved patient and 24hex char unique id  
-                res.send(res);
-            }
-            else {
-                console.log('Error in saving patient: ' + JSON.stringify(err, undefined, 2));
-            }
-        });
+    const filename2 = 'patientTmp.json';
+    const tableId2 = 'patients';
+    fs.writeFileSync(filename2, JSON.stringify(patient));         
+    const table2 = bigquery.dataset(datasetId).table(tableId2);
+    table2.load(filename2,(err2,res2) =>{
+        if (!err2) {
+            res.send(JSON.stringify(patient['_id']));
+        }
     });
+
 });
 
 module.exports = router;
