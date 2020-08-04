8c5658bd9e9c282cf7667e0c0ed98a2ce18d5913
anithanarnavaram <anithanarnavaram25@gmail.com>
2020-08-04 01:22:20 -0400

    Test Suite

---
+++ web-application/node/tests/contactusTestSuite.js
@@ -0,0 +1,21 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+const should = chai.expect;
+chai.use(chaiHttp);
+
+describe('/POST a query', () => {
+  it('create a query', () => {
+    let queryPost = {
+      'fname': 'John',
+      'lname': 'Doe',
+      'email': 'johndoe@gmail.com',
+      'message': 'Hello'
+      }
+      chai.request('http://localhost:8080')
+          .post('/')
+          .send(queryPost)
+          .end((err, res) => {
+          });
+      });
+  });
