51212090b91c9c91e2f4d21d232d245d006170c8
Srikar_Pothumahanti <srikarrohit@gmail.com>
2020-08-16 12:21:46 -0400

    param added in prod env

--- web-application/angular/src/environments/environment.prod.ts
+++ web-application/angular/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   serverUrl: "https://scriptchain-259015.appspot.com/",
-  port: 8080
+  port: 8080,
+  param: "?API_KEY=scriptChain@13$67ahi1"
 };
