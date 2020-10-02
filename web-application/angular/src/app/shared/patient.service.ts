8936847acd71d4d9831769b54747fdf169d06dda
Srikar_Pothumahanti <srikarrohit@gmail.com>
2020-10-02 13:13:52 -0400

    minor changes

--- web-application/angular/src/app/shared/patient.service.ts
+++ web-application/angular/src/app/shared/patient.service.ts
@@ -47,4 +47,9 @@ export class PatientService {
     const obj = {'user':user}
     return this.http.post(this.checkURL+environment.param,obj);
   }
+
+  search(lastname,firstname,dob){
+    const headers = {"Authorization":"Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ1cm46b2lkOmZoaXIiLCJjbGllbnRfaWQiOiJkOWYwN2JlNi0yOGNkLTQ2OWEtYjJjMS1jNjU5NWNjODE5MDEiLCJlcGljLmVjaSI6InVybjplcGljOlVTQ0RJLW9uLUZISVIiLCJlcGljLm1ldGFkYXRhIjoicXRjeG8wNlpXTzB4UlhRb044ZjRzcWJDUGxJWUt6NGxocTM3UHE1R0w0N0VKekdCUVJkSjVCUmRacFRGUmRHRU9CeE9TTHdnd3p3ZXlzTVB3T0Q1eDRMd08ySEtEYzNqSzJjSXBWYkJwNWJYbWtxOTBIM0g2Sm9qT3ZyUG9ORWwiLCJlcGljLnRva2VudHlwZSI6ImFjY2VzcyIsImV4cCI6MTYwMTQwMjg4MywiaWF0IjoxNjAxMzk5MjgzLCJpc3MiOiJ1cm46b2lkOmZoaXIiLCJqdGkiOiJjZjVkYzNkMy1iZDBjLTQxOTktOWU4Ni1lNWQ2ZmVhZjI5ZDMiLCJuYmYiOjE2MDEzOTkyODMsInN1YiI6ImV4Zm82RTRFWGpXc25oQTFPR1ZFbGd3MyJ9.CxP3Q19Q5xNT65odULbHqGhf7Dfy7sCLOgkPFq1kyCwjLe7Dyjj_A8m0qwVt4GbifKu9wo3qwddnmd2koCTVsqXEnUPJwgzvlbk9e9K3yTzaMVP69ceCqbSxy4M4Z4LDcyGCQm0E1jtg5U07RSMbQpTzxdp5nvVXgRkIyl3A0PcSW3sQtrGy0hE7PzX6EUXwr1-u32ynNJL2UBNd6N-0MV3NQ3m8CVuXvnbmjyK9CHJaUZesvA8m_d1TZ27r8eXjeXjoz4ps5R6upNoKYPk_SD_nwcPdofSOsV_AhHda8zV8CgAY7X23-MmKDR2-ztt8MFSlmbppDSGCWC9G6HxydQ"};
+    return this.http.get("https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/DSTU2/Patient?family="+lastname+"&given="+firstname+"&birthdate="+dob,{headers});
+  }
 } 
