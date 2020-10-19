60af6fc542eacb1627fd7402a897a85aac70c06c
Srikar_Pothumahanti <srikarrohit@gmail.com>
2020-10-19 18:27:56 -0400

    changes

--- Django REST API/api/predictor/EHRInterface/EpicInterface.py
+++ Django REST API/api/predictor/EHRInterface/EpicInterface.py
@@ -5,7 +5,6 @@ import os
 import subprocess
 from predictor.EHRInterface import mmlrestclient as mml
 
-
 token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ1cm46b2lkOmZoaXIiLCJjbGllbnRfaWQiOiJkOWYwN2JlNi0yOGNkLTQ2OWEtYjJjMS1jNjU5NWNjODE5MDEiLCJlcGljLmVjaSI6InVybjplcGljOlVTQ0RJLW9uLUZISVIiLCJlcGljLm1ldGFkYXRhIjoibE91eEk5bXlvZVhXWVFIdEhmNk1nYjRuUmQ3Nm54clg5bkhBZ1ZsLWxQMEJoXzFQXzhLeUVOd0RVM1FpNUdkNm94clhJdmdsVC04dG0yNEUxQS1HZ2htQ3M3ZENNRzhnNnRaSjJVdTlrTHBZVkx3S19EdmlET2wyaVExQ0VlcWEiLCJlcGljLnRva2VudHlwZSI6ImFjY2VzcyIsImV4cCI6MTU5NzA4MjI0OCwiaWF0IjoxNTk3MDc4NjQ4LCJpc3MiOiJ1cm46b2lkOmZoaXIiLCJqdGkiOiJjNWE3YjBlMi1hODA0LTRkYTAtYTcxOC01Zjg3NTM3YWZkODAiLCJuYmYiOjE1OTcwNzg2NDgsInN1YiI6ImV4Zm82RTRFWGpXc25oQTFPR1ZFbGd3MyJ9.TvYRTcXpd3J_VbKpgAClRHKkAw7GCGUMEA9pKC6B4cpj5PBlOmmnJxxgAr4-m7qKQ8UFH4osLGxyCdmCkMN6VIo2qtfcXeHSW8UcC3F5vpsDDU86XuE9aifKTJ-Hk-Nr1OoT7btW8jjV5wfqh0yaR6w47a7Z7JOFd9ndj3AHfQDGE7wgoPeoCaQxtjRBIIO3uO-DMhB9RZv8R092pBfWb1zpMZZeLS9vqbHEhDygXhvis7yqcuHGW4n34Y_hdj_nSLkA04SDXbqpXOLDFT0lbKSmMjBXjH8a3uvIi1N0a0cY4O8U7X3kOnTkq8vdPhRjZAn0CPDFh5Okim4LNcJ8CQ"  # Needs to come from FE
 
 
@@ -59,26 +58,23 @@ lab_events_url = "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/Obse
 print(fetch_lab_events(lab_events_url,
                        "e63wRTbPfr1p8UW81d8Seiw3", token, "laboratory"))
 
-def parseMedications(text):
-    f = open("./predictor/test/test.txt", "w")
-    f.write(text)
-    f.close()
-    output = mml.main()
-    content_arr = output.splitlines()
-    matchingmedic=[]
-    medications = ['phsu', 'antb', 'orch', 'inch', 'imft', 'topp', 'clnd']
-    for medic in medications:
-        matchingmedic = [s for s in content_arr if medic in s]
-    return matchingmedic
-
-def parseNotes(text):
-    f = open("./predictor/test/test.txt", "w")
-    f.write(text)
-    f.close()
-    output = mml.main()
-    content_arr = output.splitlines()
-    matching=[]
+def parseText():
+    #os.chdir("./metamap")
+    #f = open("test/srikar.txt", "w")
+    #f.write("Kidney problem is present in the patient")
+    #f.close()
+    #subprocess.run(["./metamaplite.sh","--indexdir=data/ivf/2020AA/USABase","test/srikar.txt","--overwrite"]) 
+    #f = open("test/srikar.mmi", "r")
+    args = "https://ii-public2.nlm.nih.gov/metamaplite/rest/annotate test/srikar.txt --output test/srikar3 --docformat sldiwi --resultformat mmi"
+    mml.main(args)
+    '''with open("test/srikar.mmi") as f:
+        content = f.readlines()
     doc_notes = ['acab','anab','comd','cgab','dsyn','inpo','mobd','neop','sosy']
     for note in doc_notes:
-        matching = [e for e in content_arr if note in e]
-    return matching
+        matching = [s for s in content if note in s]
+    print(matching)
+    medications = ['phsu', 'antb', 'orch', 'inch', 'imft', 'topp', 'clnd']
+    for medic in medications:
+        matchingmedic = [s for s in content if medic in s]
+    print(matchingmedic)
+    f.close()'''
--- Django REST API/api/predictor/views.py
+++ Django REST API/api/predictor/views.py
@@ -18,7 +18,6 @@ import logging
 from pprint import pprint
 from django.conf import settings
 from predictor.EHRInterface import EpicInterface as epic
-from predictor.EHRInterface import Distron as distron
 import os
 import subprocess
 
@@ -59,9 +58,29 @@ logger = logging.getLogger(__name__)
 
 
 def hello_world(request):
-    #text = "Txt001|Written informed consent.\nTxt002|Age 18 years or older.\nTxt003|informed consent.\nTxt004|Male or female."
-    #text = "Txt001|Kidney problem is there"
-    distron.generate_analytics()
+    '''
+    print('In Base URL Echo')
+    #context = {"data": "Hello World!"}
+    os.chdir("./metamap")
+    f = open("test/srikar.txt", "w")
+    f.write("Kidney problem is present in the patient")
+    f.close()
+    subprocess.run(["./metamaplite.sh","--indexdir=data/ivf/2020AA/USABase","test/srikar.txt","--overwrite"]) 
+    #f = open("test/srikar.mmi", "r")
+    with open("test/srikar.mmi") as f:
+        content = f.readlines()
+    doc_notes = ['acab','anab','comd','cgab','dsyn','inpo','mobd','neop','sosy']
+    for note in doc_notes:
+        matching = [s for s in content if note in s]
+    print(matching)
+    medications = ['phsu', 'antb', 'orch', 'inch', 'imft', 'topp', 'clnd']
+    for medic in medications:
+        matchingmedic = [s for s in content if medic in s]
+    print(matchingmedic)
+    f.close()
+    return HttpResponse("Hello World!") 
+    '''
+    epic.parseText()
     return HttpResponse("Hello World!") 
 
 
