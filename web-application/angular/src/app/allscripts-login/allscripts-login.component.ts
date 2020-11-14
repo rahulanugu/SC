a509548604fa372c0b408808304a820b538a4aff
lallen <lallen@pop-os.localdomain>
2020-11-14 10:18:32 -0800

    add allscripts link

--- web-application/angular/src/app/allscripts-login/allscripts-login.component.ts
+++ web-application/angular/src/app/allscripts-login/allscripts-login.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, Validators } from '@angular/forms';
+import { HealthcareLoginService } from '../shared/healthcare-login.service';
+import { Router } from '@angular/router';
+import { HealthcareEditService } from '../shared/healthcare-edit.service';
+
+export  class HealthcareLoginDetails{
+  username: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-allscripts-login',
   templateUrl: './allscripts-login.component.html',
@@ -7,9 +17,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllscriptsLoginComponent implements OnInit {
 
-  constructor() { }
+  healthcareLoginDetails = new HealthcareLoginDetails();
+
+
+
+  constructor(
+    private formBuilderService: FormBuilder,
+    private healthcareLoginService: HealthcareLoginService,
+    private healthcareEditService: HealthcareEditService,
+    private router:Router,
+    
+    ) { }
 
   ngOnInit() {
   }
 
+  Form = this.formBuilderService .group({
+    emailAddress: ["", Validators.required],
+    password: ["", Validators.required]
+  });
+
+  onSubmit(){
+    console.log(this.Form.value);
+    this.healthcareLoginService.healthcareProviderLogin(this.Form.value).subscribe(
+      res => {
+        //console.log("test");
+        localStorage.setItem('token',res["idToken"])
+        localStorage.setItem('fname',res["firstName"])
+        localStorage.setItem('email', this.Form.value.emailAddress)
+        //window.location.href = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fwww.scriptchain.co%2Fhealthcare-profile&client_id=788a5f45-8fcc-4ad9-bce6-e7eeefc8ac41"
+        window.location.href = "https://tw171.open.allscripts.com/authorization/connect/authorize?response_type=code&client_id=1754374B-E804-4E52-B3A2-4EA007561988&redirect_uri=https://scriptchain.co/healthcare-profile&client&scope=launch%20openid%20profile"
+             
+        //this.router.navigate(['healthcare-profile'])
+      },
+      err => {
+        console.log(err)
+        //console.log("Error is")
+        //console.log(err)
+        if(err.status == 401){
+          document.querySelector('#emailAddress').classList.remove('is-invalid');
+          document.querySelector('#invalidEmailPrompt').classList.add('d-none');    
+          document.querySelector('#password').classList.add('is-invalid');
+          document.querySelector('#invalidPasswordPrompt').classList.remove('d-none');    
+          document.querySelector('#deactivatedEmail').classList.add('d-none');
+
+
+        }else if(err.status == 303){
+          //console.log("deactivated email handling")
+          //send a reactivare mail
+          this.healthcareEditService.makeReactivateRequest({email : this.Form.value.emailAddress}).subscribe(
+            response => {
+              //console.log("response is recieved")
+              document.querySelector('#deactivatedEmail').classList.remove('d-none');
+            },
+            error => {
+              //console.log("error is recieved")
+              this.router.navigate['error500']
+            }
+          );
+        } else {
+          //console.log("errorcode")
+          document.querySelector('#invalidPasswordPrompt').classList.add('d-none');    
+          document.querySelector('#emailAddress').classList.add('is-invalid');
+          document.querySelector('#password').classList.add('is-invalid');
+          document.querySelector('#invalidEmailPrompt').classList.remove('d-none');    
+          document.querySelector('#deactivatedEmail').classList.add('d-none');
+
+        }
+      }
+    )
+
+  }
+
 }
+
+
+
+
+
