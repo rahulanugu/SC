906437819abbc9ce7fdfd045f4f27462f3b5fae7
lallen <lallen@pop-os.localdomain>
2020-11-21 14:36:06 -0800

    remove allscripts login

--- web-application/angular/src/app/app.module.ts
+++ web-application/angular/src/app/app.module.ts
@@ -72,14 +72,12 @@ import { HealthcareManageProfileComponent } from './healthcare-manage-profile/he
 import { ReactivateHealthcareProviderComponent } from './reactivate-healthcare-provider/reactivate-healthcare-provider.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dialog.component';
-import { AllscriptsLoginComponent } from './allscripts-login/allscripts-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     AthenaLoginComponent,
-    AllscriptsLoginComponent,
     HomeComponent,
     RequestAccessComponent,
     PatientComponent,
