509451024dbbb2aabd8268e724978227b10a01d3
devina8 <jaiswal.de@husky.neu.edu>
2021-02-14 22:36:16 -0500

    sign up  change

--- web-application/angular/src/app/healthcare-register/healthcare-register.component.ts
+++ web-application/angular/src/app/healthcare-register/healthcare-register.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
-import { HealthcareAccountService } from '../shared/healthcare-account.service';
-import { Router } from '@angular/router';
-import { CustomValidator } from '../shared/validators/validation';
-
+import { HealthcareAccountService } from "../shared/healthcare-account.service";
+import { Router } from "@angular/router";
+import { CustomValidator } from "../shared/validators/validation";
 
 /**
  * Page: Registeration page for the healthcare providers
@@ -11,12 +10,11 @@ import { CustomValidator } from '../shared/validators/validation';
  * Users will recieve an email from which they can verify their account before logging in.
  */
 @Component({
-  selector: 'app-healthcare-register',
-  templateUrl: './healthcare-register.component.html',
-  styleUrls: ['../app.component.css']
+  selector: "app-healthcare-register",
+  templateUrl: "./healthcare-register.component.html",
+  styleUrls: ["./healthcare-register.component.css"],
 })
 export class HealthcareRegisterComponent implements OnInit {
-
   public phonemask = [
     "(",
     /[0-9]/,
@@ -31,7 +29,7 @@ export class HealthcareRegisterComponent implements OnInit {
     /\d/,
     /\d/,
     /\d/,
-    /\d/
+    /\d/,
   ];
 
   passwordNotMatch: boolean = false;
@@ -40,13 +38,13 @@ export class HealthcareRegisterComponent implements OnInit {
     private formBuilderService: FormBuilder,
     private healthCareAccountService: HealthcareAccountService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
-    document.getElementById('registersuccessful').style.display = "none";
+    document.getElementById("registersuccessful").style.display = "none";
   }
 
-  pattern1 =  "^[0-9_-]{10,12}";
+  pattern1 = "^[0-9_-]{10,12}";
 
   Form = this.formBuilderService.group({
     firstName: ["", Validators.required],
@@ -56,23 +54,25 @@ export class HealthcareRegisterComponent implements OnInit {
     ehr: ["", Validators.required],
     roleInCompany: ["", Validators.required],
     password: ["", Validators.required],
-    phone: ["", [Validators.required, CustomValidator.phoneValidator]]
+    phone: ["", [Validators.required, CustomValidator.phoneValidator]],
   });
-  
-  submitForm(){
-    this.healthCareAccountService.generateTokenForVerification(this.Form.value).subscribe(
-    res=>{
-      document.getElementById('registrationForm').style.display = "none";
-      document.getElementById('registersuccessful').style.display = "block";
-      window.location.hash = "registersuccesful";
-    },
-    err => {
-      if(err.status === 400){
-        this.userAlreadyExist = true;
-      } else {
-        this.router.navigate(['/home'])
-      }
-    });
 
+  submitForm() {
+    this.healthCareAccountService
+      .generateTokenForVerification(this.Form.value)
+      .subscribe(
+        (res) => {
+          document.getElementById("registrationForm").style.display = "none";
+          document.getElementById("registersuccessful").style.display = "block";
+          window.location.hash = "registersuccesful";
+        },
+        (err) => {
+          if (err.status === 400) {
+            this.userAlreadyExist = true;
+          } else {
+            this.router.navigate(["/home"]);
+          }
+        }
+      );
   }
 }
