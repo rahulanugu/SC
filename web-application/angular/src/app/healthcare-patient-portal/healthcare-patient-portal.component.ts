0d0ae6923080af33b03151febdc41282a898ae53
devina8 <jaiswal.de@husky.neu.edu>
2021-02-19 11:52:39 -0500

    change

---
+++ web-application/angular/src/app/healthcare-patient-portal/healthcare-patient-portal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-healthcare-patient-portal',
+  templateUrl: './healthcare-patient-portal.component.html',
+  styleUrls: ['./healthcare-patient-portal.component.css']
+})
+export class HealthcarePatientPortalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
