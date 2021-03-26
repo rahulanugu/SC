27dae86ad9733254d33b6c1d4fd2e52241e53ea5
root <root@ip-172-31-22-143.us-east-2.compute.internal>
2021-03-26 18:01:42 +0000

    Updated start_setup for sillent execution

--- start_setup.sh
+++ start_setup.sh
@@ -2,15 +2,15 @@ sudo su
 curl --silent --location https://rpm.nodesource.com/setup_14.x | bash -
 
 cd /home/ec2-user/app/web-application/angular
-npm install
-npm link @angular/cli
-npm install -g @angular/cli@latest
-ng update -g
+npm install -g --silent
+npm link -g --silent @angular/cli
+npm install -g --silent @angular/cli@latest
+ng update
 ng update @angular/cli @angular/core --allow-dirty --force
 npm run build -- --prod
 
 cd /home/ec2-user/app/web-application/node
-npm install
+npm install -g --silent
 node index.js
 
 cd /home/ec2-user/app/web-application/angular
