5c4aa421b198ec8331e169d217cf79cc9ee5654a
VikNim <vsn22@njit.edu>
2021-03-26 07:50:53 +0000

    File to install node and packages after deployement

--- start_setup.sh
+++ start_setup.sh
@@ -8,6 +8,11 @@ cd /home/ec2-user/app/web-application/node
 npm install
 node index.js
 
+npm link @angular/cli
+npm install -g @angular/cli@latest
+ng update
+ng update @angular/cli @angular/core --allow-dirty --force
+
 cd /home/ec2-user/app/web-application/angular
 ng serve --o
 
