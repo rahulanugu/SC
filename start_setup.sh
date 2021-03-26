24412f0c95effe6b9e67af19dc5d043e46d84e1a
root <root@ip-172-31-22-143.us-east-2.compute.internal>
2021-03-26 14:22:05 +0000

    Updated start_setup.sh

--- start_setup.sh
+++ start_setup.sh
@@ -3,16 +3,15 @@ curl --silent --location https://rpm.nodesource.com/setup_14.x | bash -
 
 cd /home/ec2-user/app/web-application/angular
 npm install
+npm link @angular/cli
+npm install -g @angular/cli@latest
+ng update -g
+ng update @angular/cli @angular/core --allow-dirty --force
 npm run build -- --prod
 
 cd /home/ec2-user/app/web-application/node
 npm install
 node index.js
-
-npm link @angular/cli
-npm install -g @angular/cli@latest
-ng update
-ng update @angular/cli @angular/core --allow-dirty --force
 
 cd /home/ec2-user/app/web-application/angular
 ng serve --o
