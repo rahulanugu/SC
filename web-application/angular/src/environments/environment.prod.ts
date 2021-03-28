3153bec03464cdefaaf54b44cd6be057661c3b71
Srikar_Pothumahanti <srikarrohit@gmail.com>
2021-03-28 13:58:39 -0500

    changes

--- web-application/angular/src/environments/environment.prod.ts
+++ web-application/angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: "http://3.16.14.209",
+  serverUrl: "http://ec2-3-16-14-209.us-east-2.compute.amazonaws.com",
   port: 3000,
   param: "?API_KEY=TiKY7Md2dHpcZo1ih4KbkinTHh7CNTSjseg2ZB3ZiaEC2x1bFA=="
 };
