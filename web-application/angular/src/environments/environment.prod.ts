ab7a7d83398656820fa09d30fc540950e3e5465f
Srikar_Pothumahanti <srikarrohit@gmail.com>
2021-03-29 16:04:07 -0500

    changes

--- web-application/angular/src/environments/environment.prod.ts
+++ web-application/angular/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  serverUrl: "http://3.16.14.209",
-  port: 3000,
+  //serverUrl: "https://3.16.14.209",
+  serverUrl: "https://scriptchain.co",
+  //port: 3000,
   param: "?API_KEY=TiKY7Md2dHpcZo1ih4KbkinTHh7CNTSjseg2ZB3ZiaEC2x1bFA=="
 };
