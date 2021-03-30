549571be763009ada199c5a65617939c0c46492a
Srikar_Pothumahanti <srikarrohit@gmail.com>
2021-03-30 09:00:07 -0500

    changes

--- start_setup.sh
+++ start_setup.sh
@@ -1,4 +1,4 @@
-cd /home/ec2-user/app/web-application/angular
+#cd /home/ec2-user/app/web-application/angular
 #npm install -g --silent
 #npm link -g --silent @angular/cli
 #npm install -g --silent @angular/cli@latest
@@ -6,6 +6,6 @@ cd /home/ec2-user/app/web-application/angular
 #ng update @angular/cli @angular/core --allow-dirty --force
 #npm run build -- --prod
 
-cd /home/ec2-user/app/web-application/node
+#cd /home/ec2-user/app/web-application/node
 #npm install -g --silent
-sudo pm2 start index.js
+#sudo pm2 start index.js
