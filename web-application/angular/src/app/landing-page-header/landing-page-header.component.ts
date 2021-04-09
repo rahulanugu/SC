744278247e1134a8fe4bb3d1b5c93685a1c1f0a8
devina8 <jaiswal.de@husky.neu.edu>
2021-04-08 21:28:10 -0400

    navbar change

--- web-application/angular/src/app/landing-page-header/landing-page-header.component.ts
+++ web-application/angular/src/app/landing-page-header/landing-page-header.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-landing-page-header',
-  templateUrl: './landing-page-header.component.html',
-  styleUrls: ['./landing-page-header.component.css']
+  selector: "app-landing-page-header",
+  templateUrl: "./landing-page-header.component.html",
+  styleUrls: ["./landing-page-header.component.css"],
 })
 export class LandingPageHeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
