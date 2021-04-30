96868bbd568bf54b4806b00f3daabd536086294f
devina8 <jaiswal.de@husky.neu.edu>
2021-04-30 17:11:06 -0400

    login page fix

--- web-application/angular/src/app/becomeapartner/becomeapartner.component.ts
+++ web-application/angular/src/app/becomeapartner/becomeapartner.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { CustomValidator } from "../shared/validators/validation";
 @Component({
   selector: "app-becomeapartner",
   templateUrl: "./becomeapartner.component.html",
   styleUrls: ["./becomeapartner.component.css"],
 })
 export class BecomeapartnerComponent implements OnInit {
-  constructor() {}
+  registerForm: FormGroup;
+  submitted: false;
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      phone: ["", [Validators.required, CustomValidator.phoneValidator]],
+      companyName: ["", Validators.required],
+      message: ["", Validators.required],
+    });
+  }
 }
