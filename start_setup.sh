311e42787867196fd9f057a28cc27128770c6822
Vidit S Shah <shah444@purdue.edu>
2021-06-07 17:40:09 -0400

    testing CI/CD

--- start_setup.sh
+++ start_setup.sh
@@ -7,10 +7,10 @@
 #ng update @angular/cli @angular/core --allow-dirty --force
 #npm run build -- --prod
 
-cd /home/ec2-user/app/web-application/node
-#npm install -g --silent
-sudo pm2 stop index.js
-sudo pm2 start ecosystem.config.js
-sudo systemctl restart nginx
+# cd /home/ec2-user/app/web-application/node
+# #npm install -g --silent
+# sudo pm2 stop index.js
+# sudo pm2 start ecosystem.config.js
+# sudo systemctl restart nginx
 # cd /home/ec2-user/app/Django\ REST\ API/api
 # pm2 start manage.py --interpreter python3 -- runserver 0.0.0.0:8000
