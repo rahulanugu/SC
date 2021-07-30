0c0f912de5a408359f854369842fb80ea9f2df1c
Vidit S Shah <shah444@purdue.edu>
2021-07-30 14:10:48 -0400

    UI enhancements and update on the team headshots

--- web-application/node/index.js
+++ web-application/node/index.js
@@ -9,6 +9,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+const nodemailer = require("nodemailer");
 // local import
 var patientController = require("./controllers/patientController");
 var caregiverController = require("./controllers/caregiverController");
@@ -23,10 +24,11 @@ var healthcareProviderLoginController = require("./controllers/healthcareProvide
 var healthcareProviderResetPasswordController = require("./controllers/healthcareProviderResetPasswordController");
 var deactivateController = require("./controllers/deactivateController");
 var reactivateController = require("./controllers/reactivateController");
-var editPatientController = require("./controllers/editPatientController")
-var cacheController = require("./controllers/cacheController")
-var partnersController = require("./controllers/partnersController")
-var patientsNewController = require("./controllers/patientNewController")
+var editPatientController = require("./controllers/editPatientController");
+var cacheController = require("./controllers/cacheController");
+var partnersController = require("./controllers/partnersController");
+var patientsNewController = require("./controllers/patientNewController");
+const mailer_oauth = require('./mailer_oauth');
 
 var app = express();
 
@@ -56,13 +58,12 @@ connection.end();
 //for local deploy uncomment below code & comment out for production
 //allow cors to access port that angular app runs on
 
-
 //Comment this in local as well. Requests are not being handled
 //Also on port 8080, testing of API is bad. Console.log isn't working
 //Only when requests come from 4200(UI), console.log is working
 //Finally, it's important to serve static comment for testing of few APIs.
 
- /*app.use(
+/*app.use(
    cors({
      origin: "http://scriptchain.co"
    })
@@ -78,34 +79,39 @@ connection.end();
 //app.use(require('cookie-parser')('hosenkinosumabeni'))
 //for production mode uncomment below code and for local(local requests are handled on 8080 alone if commented)
 app.use(function (req, res, next) {
-
   //res.cookie('cookie','value',{signed:true});
   // Website you wish to allow to connect
   var whitelist = [
-    'https://www.scriptchain.co',
-    'https://scriptchain.co',
-    'http://localhost:4200',
-    'http://localhost:8080',
-    'http://localhost:3000',
-    '127.0.0.1'
+    "https://www.scriptchain.co",
+    "https://scriptchain.co",
+    "http://localhost:4200",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "127.0.0.1",
     //'http://3.16.14.209:3000'
   ];
   var origin = req.headers.origin;
   if (whitelist.indexOf(origin) > -1) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  
-  if (req.hostname != 'localhost' && req.get('X-Forwarded-Proto') == 'http') {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  if (req.hostname != "localhost" && req.get("X-Forwarded-Proto") == "http") {
     res.redirect(`https://${req.host}${req.url}`);
     return;
   }
@@ -117,7 +123,6 @@ app.use(function (req, res, next) {
 //Uncomment out the below code in production mode and local mode.
 app.use(express.static(path.join(__dirname, "./dist/my-app")));
 
-
 // add router from patient controller
 app.use("/patient", patientController);
 app.use("/caregiver", caregiverController);
@@ -128,14 +133,45 @@ app.use("/verified", verifiedController);
 app.use("/contact_us", contactUsController);
 app.use("/careers", careersController);
 app.use("/reset_password", resetPasswordController);
-app.use("/backend/healthcare",healthcareProviderController);
-app.use("/backend/healthcare-login",healthcareProviderLoginController);
-app.use("/backend/healthcare/reset_password", healthcareProviderResetPasswordController);
-app.use("/backend/deactivate", deactivateController )
-app.use("/backend/reactivate", reactivateController )
-app.use("/backend/editpatient" , editPatientController)
-app.use("/cache_service" , cacheController)
-app.use("/partners" , partnersController)
+app.use("/backend/healthcare", healthcareProviderController);
+app.use("/backend/healthcare-login", healthcareProviderLoginController);
+app.use(
+  "/backend/healthcare/reset_password",
+  healthcareProviderResetPasswordController
+);
+app.use("/backend/deactivate", deactivateController);
+app.use("/backend/reactivate", reactivateController);
+app.use("/backend/editpatient", editPatientController);
+app.use("/cache_service", cacheController);
+app.use("/partners", partnersController);
+
+const oauth2Client = mailer_oauth.getClient();
+const accessToken = oauth2Client.getAccessToken();
+
+app.post("/sendEmail", (req, res) => {
+  var data = req.body;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: "moh@scriptchain.co",
+      clientId: "867282827024-auj9ljqodshuhf3lq5n8r79q28b4ovun.apps.googleusercontent.com",
+      clientSecret: "zjrK7viSEMoPXsEmVI_R7I6O",
+      refreshToken: "1//04OyV2qLPD5iYCgYIARAAGAQSNwF-L9IrfYyKF4kF_HhkGaFjxxnxdgxU6tDbQ1l-BLlOIPtXtCDOSj9IkwiWekXwLCNWn9ruUiE",
+      accessToken: accessToken
+    },
+  });
+  const emailConfig = {
+    from: "moh@scriptchain.co",
+    to: data.email,
+    subject: data.subject,
+    html: data.template,
+  };
+  transporter.sendMail(emailConfig, (err, info) => {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+});
 
 //Uncomment out the below code in production mode and local mode.
 app.get("*", (req, res) => {
