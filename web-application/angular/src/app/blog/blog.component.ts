6ae88dc37e1aee53d274a245d52301f2d0492985
Sammy2023 <55414520+Sammy2023@users.noreply.github.com>
2021-10-05 17:39:25 -0400

    update blog-page

--- web-application/angular/src/app/blog/blog.component.ts
+++ web-application/angular/src/app/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { BlogCardComponent } from "../shared/components/blog-card/blog-card.component";
 
 @Component({
   selector: "app-blog",
@@ -6,9 +7,18 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./blog.component.css"],
 })
 export class BlogComponent implements OnInit {
-  constructor() {}
 
   ngOnInit() {}
+  blogCard = BlogCardComponent;
+  categoryList = [
+    { value: "updates", name:"Scriptchain Updates"},
+    { value: "ai", name:"AI"},
+    { value: "invest", name:"Investing in Healthcare"},
+    { value: "patient", name:"Patient Outcomes"},
+    { value: "scholar", name:"Scholarly Articles"},
+    { value: "industry", name:"What the Industry is Saying"}
+  ];
+  
 
 
 }
