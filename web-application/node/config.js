3217c6af170ab4a254233a1fcbc65a0d560812d0
TejvirSSaggu <tejvirsaggu03@gmail.com>
2022-06-16 21:54:32 -0700

    Merge fixes

--- web-application/node/config.js
+++ web-application/node/config.js
@@ -2,12 +2,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({
-<<<<<<< HEAD
-  path: path.resolve(_dirname, `${process.env.NODE_ENV}.env`)
-
-=======
   path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)});
->>>>>>> development-environment
  
  module.exports = {
               NODE_ENV : process.env.NODE_ENV || 'development',
